019a7c635407f7b002e3d5144384b16936d337d6
Aditya Malode <82941123+Ady-6720@users.noreply.github.com>
2023-05-01 12:45:24 +0530

    Method creation

---
+++ MethodCreation.java
@@ -0,0 +1,11 @@
+public class MethodCreation {
+    public static void main(String[] args){
+        Integer A=25;
+        Multiply(A);
+    }
+
+    public static void Multiply(Integer a){
+        System.out.println(a*100);
+    }
+    
+}
